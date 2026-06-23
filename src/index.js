5d9cd3639215742e7f55044fcb8b0d2c41b30e41
Shittu Emmanuel <shem4soul@gmail.com>
2026-06-23 14:03:27 +0100

    Remove React.StrictMode wrapper from App component rendering

--- src/index.js
+++ src/index.js
@@ -167,9 +167,10 @@ function Order({ closeHour, openHour }) {
 // React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <App />,
 );
 
 // React before 18
