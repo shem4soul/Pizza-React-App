b4b8c93ba7d9c47063f549d987d6ae1450d259a2
Shittu Emmanuel <shem4soul@gmail.com>
2026-06-23 11:59:09 +0100

    Refactor Pizza and Order components to improve prop handling and enhance footer messaging

--- src/index.js
+++ src/index.js
@@ -103,17 +103,17 @@ function Menu() {
   );
 }
 
-function Pizza(props) {
-  console.log(props);
+function Pizza({ pizzaObj }) {
+  console.log(pizzaObj);
 
-  if (props.pizzaObj.soldOut) return null;
+  if (pizzaObj.soldOut) return null;
   return (
     <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price}</span>
       </div>
     </li>
   );
@@ -128,23 +128,22 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <Order closeHour={closeHour} />
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
-          We're currently closed. Our working hours are from {openHour}:00 to{" "}
-          {closeHour}:00. Come visit us during the day or order online.
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
         </p>
       )}
     </footer>
   );
 }
 
-function Order(props) {
+function Order({ closeHour, openHour }) {
   return (
     <div className="order">
       <p>
-        We're currently open until {props.closeHour}:00. Come visit us or order
-        online.
+        We're currently open from {openHour}:00 to {closeHour}:00. Come visit us
+        or order online.
       </p>
       <button className="btn">Order</button>
     </div>
